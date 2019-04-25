51f6ba9cca3338e000c405215e9533d447099a42
Pedro Dutra-Vaz (Kforce) <v-pedutr@microsoft.com>
2019-04-24 21:29:52 -0700

    Added backgrounds to project sections

--- assets/js/project-pages.js
+++ assets/js/project-pages.js
@@ -17,8 +17,17 @@ function addProjectPage(project, index) {
     var page = document.createElement("div");
     var pageClasses = index % 2 == 0 ? 'project-page half-page' : 'project-page half-page right';
     page.setAttribute('class', pageClasses);
+
+    var backgroundHolder = document.createElement("div");
+    backgroundHolder.setAttribute('class', 'background');
+    var backgroundHolderInner = document.createElement("span")
+    backgroundHolderInner.setAttribute('class', 'background-inner');
+    backgroundHolderInner.setAttribute('style', 'background-image: url("assets/' + project.imageFolder + 'background.png")');
+    backgroundHolder.append(backgroundHolderInner);
+    
+    $(page).append(backgroundHolder);
     $(page).append("<div class='project-header'>" + project.title + "</div>");
-    $(page).append("<div class='project-page-img'><img src='assets/" + project.bannerImage + "'></div>")
+    $(page).append("<div class='project-page-img'><img src='assets/" + project.imageFolder + "bannerimg.png'></div>")
     $(page).append("<div class='project-description'><p class='project-info'>" + project.description + "</p></div>");
 
     $('#project-section').append(page);

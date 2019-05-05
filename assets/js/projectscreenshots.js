1487d9f01883cd54bcbbd7a20e2cc7e4388c4a05
Pedro Dutra <pedro@flizzet.com>
2019-05-05 07:31:32 -0700

    Changed color styling, fixed final screenshot being ommited from slider

--- assets/js/projectscreenshots.js
+++ assets/js/projectscreenshots.js
@@ -2,7 +2,7 @@ var currentSlider = undefined;
 
 function getProjectScreenshots(project) {
     let allScreenshots = "";
-    for (var i = 1; i < project.totalScreenshots; i++) {
+    for (var i = 1; i < project.totalScreenshots + 1; i++) {
         var screenshotURL = "assets/" + project.imageFolder + "screenshots/" + i + ".png";
         let imageSlide = 
             `<li data-thumb="` + screenshotURL + `" data-src="` + screenshotURL + `">

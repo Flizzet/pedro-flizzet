c9675ff73526d219e15e47502f6630f4a689aaf8
Pedro Dutra <pedro@flizzet.com>
2019-05-17 16:34:27 -0700

    Added stretch to left half with mouse

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,32 +1,46 @@
+var introClosed = false;
+
 $(document).ready(function() {
 
-    $('.scroll-down-indicator').on('click', function() {
-        console.log("Click");
-        // $('#start-page').addClass('closed');
+    // Moving left half horizontally on mouse movement
+    $('#start-page .main-page').mousemove(function(event) {
+        stretchLeftHalf(event);
+    });
 
-        // $('#start-page').fadeOut(700);
-
-        $('#start-page').animate({
-            left: "+=100vw"
-        }, 500, "swing", function() {
-            console.log("Callback");
-            // $(this).remove();
-            // $('html').css('overflow-y', 'visible');
-            // $('body').css('overflow-y', 'visible');
-        })
-        setTimeout(function() {
-            $('#main').animate({
-                left: "+=100vw"
-            }, 400, "easeOutQuart", function() {
-
-            });
-        }, 300)
-
-        // $('#start-page').children().fadeOut(300);
-        // $('#start-page').children().css('opacity', '0');
-        // $('#main').addClass('open');
-        // $('#start-page .uncover-container').css('display', 'none');
-        // $('#main').css('display', 'block');
-    })
+    // Closing intro & opening main page on scrolldown or button click
+    $('.scroll-down-indicator').on('click', closeIntro);
 
 });
+
+function closeIntro() {
+    if (introClosed) return;
+
+    introClosed = true;
+
+    // Close intro page
+    $('#start-page').animate({
+        left: "+=100vw"
+    }, 500, "swing", function() {
+        // Intro page finished swiping away
+    });
+    // Opem main page
+    setTimeout(function() {
+        $('#main').animate({
+            left: "+=100vw"
+        }, 400, "easeOutQuart", function() {
+            // Main page finished swiping in
+        });
+    }, 300);
+
+}
+
+function stretchLeftHalf(mouseData) {
+    var leftHalf = $('#start-page .main-page .left-half');
+    // Get difference between mouse and leftHalf
+    var mouseDistance = mouseData.pageX - leftHalf.offset().left;
+    var stretchStrength = 0.02;
+    var stretchAmt = mouseDistance * stretchStrength;
+    
+    // Apply newly found stretch to left half
+    leftHalf.css('margin-left', stretchAmt);
+}
